088abc3d0ea0fde4d6dd29c9bf1f27683d9b2c28
Vrock691 <vrock691@gmail.com>
2024-05-23 15:26:23 +0200

    feat: Ajout des assets des projets

---
+++ app/lib/projects.ts
@@ -0,0 +1,32 @@
+const projects = [
+    {
+        iconPath: null,
+        name: "RadioTalk",
+        desc: "RadioTalk est un mini projet qui permet à deux appareils d'échanger des fichiers audio à la manière d'un talkie walkie.",
+        languages: ["Flutter, NodeJS, Socket.io"],
+        source: "https://github.com/Vrock691/RadioTalk",
+        website: null,
+        store: null,
+        people: null,
+    },
+    {
+        iconPath: null,
+        name: "SCO2",
+        desc: "RadioTalk est un mini projet qui permet à deux appareils d'échanger des fichiers audio à la manière d'un talkie walkie.",
+        languages: ["Flutter, NodeJS, Socket.io"],
+        source: "https://github.com/Vrock691/RadioTalk",
+        website: null,
+        store: null,
+        people: []
+    },
+    {
+        iconPath: null,
+        name: "SCO2",
+        desc: "RadioTalk est un mini projet qui permet à deux appareils d'échanger des fichiers audio à la manière d'un talkie walkie.",
+        languages: ["Flutter, NodeJS, Socket.io"],
+        source: "https://github.com/Vrock691/RadioTalk",
+        website: null,
+        store: null,
+        people: []
+    },
+]
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Projects() {
+    const [filter, setFilter] = useState('');
+
     return (
       <main className="">
-        <h1>Projets</h1>
+        <h1 className='text-xl'>Projets</h1>
+        <br />
+        <p>Voici une liste non-exaustive de mes derniers projets personnels.</p>
+        
       </main>
     );
   }
