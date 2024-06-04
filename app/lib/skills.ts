3f9b1bfcd9ca30c1a8cd8ba5d8c7114db098d53e
Valentin Mary <39530176+Vrock691@users.noreply.github.com>
2024-06-04 12:22:52 +0000

    add: Ajouts de nouveaux skills

--- app/lib/skills.ts
+++ app/lib/skills.ts
@@ -1,4 +1,4 @@
 export const languages = ["HTML", "CSS", "JS", "Dart", "SQL", "Python", "C", "Bash Script"];
 export const lib_framework = ["React", "TailwindCSS", "NextJS", "NodeJS", "Express", "Flutter"];
-export const providers = ["Firebase", "Google Play Store", "Stripe", "Domaines OVH", "Simple Analytics"];
-export const OS_software = ["Git", "Github", "Nginx", "SSH", "Fedora", "Debian", "VScode", "IntelliJ Android Studio", "Postman", "Figma"];
+export const providers = ["Firebase", "Google Play Store", "Stripe", "Domaines OVH", "Simple Analytics", "Meta Business Suite", "Vercel"];
+export const OS_software = ["Git", "Github", "Nginx", "SSH", "Fedora", "Debian", "VScode", "IntelliJ Android Studio", "Postman", "Figma", "Adobe Express", "Illustrator"];
